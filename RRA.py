98fe4329ef119a9aa9f2352d59f715e35887c38b
Harsh Saini <100159384+harrshsaini@users.noreply.github.com>
2023-09-28 18:58:42 +0530

    Update RRA.py

--- RRA.py
+++ RRA.py
@@ -1,33 +1,63 @@
-import numpy as np
 import pandas as pd
-dataset = pd.read_csv('Restaurant_Reviews.tsv',delimiter='\t')
-import re
-import nltk
-nltk.download('stopwords')
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
-corpus=[]
-
-for i in range(0,1000):
-  review = re.sub('[^a-zA-Z]',' ',dataset['Review'][i])
-  review = review.lower()
-  review = review.split()
-  ps = PorterStemmer()
-  review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
-  corpus.append(review)
-  
-from sklearn.feature_extraction.text import CountVectorizer
-cv = CountVectorizer(max_features = 1500)
-X = cv.fit_transform(corpus).toarray()
-y = dataset.iloc[:,1].values 
-
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_trian, y_test = train_test_split(X,y,test_size = 0.25)
-from sklearn.ensemble import RandomForestClassifier
-model = RandomForestClassifier(n_estimators=501,criterion='entropy')
-model.fit(x_train,y_train)
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score, recall_score
 
-y_pred = model.predict(x_test)
-print(y_pred)
-acc = round(model.score(x_test,y_test)*100,2)
-print(str(acc)+'%')
+
+
+ds=pd.read_csv('Restaurant_Reviews.tsv', delimiter='\t')
+
+ds.shape,ds.columns
+
+X_train, X_test, y_train, y_test = train_test_split(ds['Review'], ds['Liked'], test_size=0.2, random_state=42)
+
+
+
+tfidf_vectorizer = TfidfVectorizer(max_features=1000)
+X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
+X_test_tfidf = tfidf_vectorizer.transform(X_test)
+
+
+svm_classifier = SVC(kernel='linear', C=1.0)
+
+svm_classifier.fit(X_train_tfidf, y_train)
+
+y_pred = svm_classifier.predict(X_test_tfidf)
+
+y_pred
+
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+
+print(f"Accuracy: {accuracy:.2f}")
+print("Classification Report:\n", report)
+
+
+precision =round(precision_score(y_test, y_pred)*100,2)
+recall = round(recall_score(y_test, y_pred)*100,2)
+
+
+print(f"Precision: {precision:.2f}")
+print(f"Recall: {recall:.2f}")
+
+
+cp = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:\n", cp)
+
+cp = confusion_matrix(y_test, y_pred)
+
+precision = round(precision_score(y_test, y_pred)*100,2)
+recall = round(recall_score(y_test, y_pred)*100,2)
+accuracy = round(accuracy_score(y_test, y_pred)*100,2)
+
+print(f"Accuracy: {accuracy:.2f}")
+print(f"Precision: {precision:.2f}")
+print(f"Recall: {recall:.2f}")
